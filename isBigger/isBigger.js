bb0ed8d71d70e07a3ffdd4b2ca7c8fa01040f611
cvc1230 <chrissyvc@anniecannons.com>
2020-11-05 10:17:08 -0800

    no message

--- isBigger/isBigger.js
+++ isBigger/isBigger.js
@@ -2,6 +2,17 @@
 Given two numbers, a, b, as inputs, return true if a > b,
 return false if a < b, return 0 otherwise.
 
+declare a function
+	takes two inputs such as parameters/arguments
+	if/else conditionals
+		is a bigger than b?
+		is a smaller than b?
+		is a equal to b?
+
+Output : "true", "false", 0
+
+call the function, pass in 2 arguments
+
 Bonus 1: If a or b is undefined or not a number, then return -1.
 Hint: Chekout typeof() operator at
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
@@ -40,6 +51,17 @@ console.log(result) // -1
 
 
 function isBigger(a, b) {
-	//Your code here
+  
+	if (a > b) {
+    console.log("true");
+  } else if (a < b) {
+    console.log("false");
+  } else {
+    console.log("0 otherwise")
+  } 
 	
 }
+
+isBigger(2,3);
+isBigger(5,5);
+isBigger(4,3);
